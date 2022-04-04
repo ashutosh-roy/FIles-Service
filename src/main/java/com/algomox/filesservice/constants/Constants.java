8af390c76e480af3ee5a4198af499f23d0e83021
Roy <AH40299@ad.wellpoint.com>
2022-04-04 09:53:54 +0530

    Controller added for GET API.

---
+++ src/main/java/com/algomox/filesservice/constants/Constants.java
@@ -0,0 +1,17 @@
+package com.algomox.filesservice.constants;
+
+public class Constants {
+
+	public static final String FILES = "/files";
+	public static final String V1 = "/v1";
+	public static final String EMPTY_STRING = "";
+	public static final String FILES_SERVICE = "Files Service";
+	public static final String FAILURE = "FAILURE";
+	public static final String SUCCESS = "SUCCESS";
+	public static final String DOTSTRING = ".";
+	public static final String TECHNICAL = "TECHNICAL";
+	public static final String T_001 = "T_001";
+	public static final String T_002 = "T_002";
+	public static final String T_003 = "T_003";
+	public static final String FILE_NAME_MISSING_ERROR = "File Name cannot be empty.";
+}
--- src/main/java/com/algomox/filesservice/controller/FilesController.java
+++ src/main/java/com/algomox/filesservice/controller/FilesController.java
@@ -1,5 +1,49 @@
 package com.algomox.filesservice.controller;
+import java.util.List;
 
-public class FilesController {
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algomox.filesservice.constants.Constants;
+import com.algomox.filesservice.exception.WEBPersistenceException;
+import com.algomox.filesservice.models.FilesApiResponse;
+import com.algomox.filesservice.service.FilesService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value = Constants.V1 + Constants.FILES)
+public class FilesController 
+{	
+	@Autowired 
+	FilesService files;
+	
+	ObjectMapper ob = new ObjectMapper();
+	
+	@GetMapping()
+	public FilesApiResponse readFile(@RequestParam String fileName) throws Exception
+	{
+		if(StringUtils.isEmpty(fileName)) 
+		{
+			return files.readFile(fileName);
+		}
+		else
+		{
+			throw new WEBPersistenceException(Constants.FILE_NAME_MISSING_ERROR);
+		}
+		
+	}
+	
+	@PostMapping()
+	public String saveFile(List<Object> fileData)
+	{
+		return null;
+	}
 
 }
